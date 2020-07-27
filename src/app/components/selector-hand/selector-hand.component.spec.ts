e6d6880aafc1efa48a52fb335f19563d9db5dd61
Quangdao Nguyen <quangdaon@gmail.com>
2020-07-26 19:28:31 -0500

    Fix unit tests

--- src/app/components/selector-hand/selector-hand.component.spec.ts
+++ src/app/components/selector-hand/selector-hand.component.spec.ts
@@ -1,20 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectorHandComponent } from './selector-hand.component';
+import { SelectorComponent } from '../selector/selector.component';
+import { Component } from '@angular/core';
+import hands, { Hand, SelectableHand } from 'src/app/data/hands';
 
 describe('SelectorHandComponent', () => {
-  let component: SelectorHandComponent;
-  let fixture: ComponentFixture<SelectorHandComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  const hand: SelectableHand = {
+    ...hands.rock,
+    disabled: false,
+    selected: false
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SelectorHandComponent ]
+      declarations: [ SelectorHandComponent, TestHostComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SelectorHandComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +30,13 @@ describe('SelectorHandComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+
+  @Component({
+    selector: `host-component`,
+    template: `<app-selector-hand [hand]="hand"></app-selector-hand>`
+  })
+  class TestHostComponent {
+    hand: SelectableHand = hand
+  }
 });
